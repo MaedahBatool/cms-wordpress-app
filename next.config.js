20fb4a3b23591385283bef3522841c8344e47f0a
Maedah Batool <me@MaedahBatool.com>
2022-08-05 18:59:04 +0500

    First draft

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
+	throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `)
+  `);
 }
 
 module.exports = {
-  images: {
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(http(?:s)?:\/\/)(.*)/)[2], // Valid WP Image domain.
-      '2.gravatar.com',
-      'secure.gravatar.com',
-    ],
-  },
-}
+	images: {
+		domains: [
+			process.env.WORDPRESS_API_URL.match(/(http(?:s)?:\/\/)(.*)/)[2], // Valid WP Image domain.
+			'0.gravatar.com',
+			'secure.gravatar.com',
+			'nextwp.local',
+		],
+	},
+};
